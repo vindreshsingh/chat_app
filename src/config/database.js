06e67191aa324eab496912dfef721fee22d000fa
Vindresh.Kumar <vindresh.kumar@DC4Y0GF219.local>
2025-07-18 02:57:40 +0530

    fix: remove database uri from my commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-//const URI = "mongodb+srv://vindresh_singh:vindresh_singh@vindreshsingh.js69iij.mongodb.net/ChatApp"
 
 const connectDatabase = async () => {
     await mongoose.connect(process.env.URI)
